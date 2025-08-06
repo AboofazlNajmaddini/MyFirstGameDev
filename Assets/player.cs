be7b988d05f2eb828bb2022c6a75bf3a0fe667f8
AboofazlNajmaddini <Aboofazl.najmaddini1@gmail.com>
2025-08-06 20:41:29 +0330

    commit

--- Assets/player.cs
+++ Assets/player.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
     private float xinput;
 
-    [SerializeField]private float moveSpeed;
-    [SerializeField]private float jampforce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jampforce;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +18,12 @@ public class player : MonoBehaviour
     void Update()
     {
         xinput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xinput*moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(xinput * moveSpeed, rb.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space)) // Check if the space key is pressed down
         {
             rb.velocity = new Vector2(rb.velocity.x, jampforce); // Reset vertical velocity to prevent double jump
         }
 
-    } 
+    }
 
 }
